398c89490c941de578a244c303436e7de7e41e8d
Moeishere <hhassan@csus.edu>
2024-04-25 17:39:29 -0700

    Added Start Screen with options Play Game, Leaderboard, Credits Buttons. Changed Background to match the theme.

--- app/src/main/java/com/gamecodeschool/c17snake/StartScreenActivity.java
+++ app/src/main/java/com/gamecodeschool/c17snake/StartScreenActivity.java
@@ -1,10 +1,11 @@
 package com.gamecodeschool.c17snake;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
+
 
 public class StartScreenActivity extends Activity {
 
@@ -30,7 +31,25 @@ public class StartScreenActivity extends Activity {
 
     // Called when the "Credits" text is clicked
     public void onCreditsClick(View view) {
-        // Code to show credits
+        AlertDialog.Builder creditsDialog = new AlertDialog.Builder(this);
+        creditsDialog.setTitle("Credits");
+
+        String message = "This is a basic 2D snake game that has been greatly improved with the principles of OOP. " +
+                "We have added numerous features and enhanced the appearance to bring you a better gaming experience. " +
+                "\n\nFinal Project Group 34:\n" +
+                "- Muhammad Hassan\n" +
+                "- Brandon Symansiv\n" +
+                "- Huy Tran\n" +
+                "- Kevin Esquivel\n" +
+                "- Steven Graham";
+
+        creditsDialog.setMessage(message);
+        creditsDialog.setPositiveButton("OK", (dialog, id) -> {
+        });
+
+        AlertDialog dialog = creditsDialog.create();
+        dialog.show();
     }
+
 }
 
