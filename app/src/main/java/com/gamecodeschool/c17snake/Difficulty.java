509531557c2b6486e70f312fc4f222e17ffb7300
HuyTran <huyqtran@csus.edu>
2024-05-09 14:39:28 -0700

    Added Potion - use it for powerup snake

--- app/src/main/java/com/gamecodeschool/c17snake/Difficulty.java
+++ app/src/main/java/com/gamecodeschool/c17snake/Difficulty.java
@@ -4,7 +4,7 @@ public class Difficulty {
     private int speed;
     private int obstacleFrequency;
     private int appleFrequency;
-    private int eggFrequency;
+    private int potionFrequency;
 
     public Difficulty(int level) {
         switch (level) {
@@ -12,19 +12,19 @@ public class Difficulty {
                 this.speed = 5;
                 this.appleFrequency = 3;
                 this.obstacleFrequency = 1;
-                this.eggFrequency = 3;
+                this.potionFrequency = 3;
                 break;
             case 1: // Medium
                 this.speed = 10;
                 this.appleFrequency = 2;
                 this.obstacleFrequency = 2;
-                this.eggFrequency = 2;
+                this.potionFrequency = 2;
                 break;
             case 2: // Hard
                 this.speed = 15;
                 this.appleFrequency = 1;
                 this.obstacleFrequency = 3;
-                this.eggFrequency = 1;
+                this.potionFrequency = 1;
                 break;
             }
     }
@@ -41,7 +41,7 @@ public class Difficulty {
         return appleFrequency;
     }
     public int getPotionFrequency() {
-        return appleFrequency;
+        return potionFrequency;
     }
 
 }
